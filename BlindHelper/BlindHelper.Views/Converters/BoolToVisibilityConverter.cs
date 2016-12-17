6aa45895e090efdebf8f2c174e95de4d64135075
SerafimovichEugene <danmer89@gmail.com>
2016-12-17 13:06:45 +0300

    SpeechProvider
    SpeechProvider and switch-case for 9 areas of frame.

--- BlindHelper/BlindHelper.Views/Converters/BoolToVisibilityConverter.cs
+++ BlindHelper/BlindHelper.Views/Converters/BoolToVisibilityConverter.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
+
 namespace BlindHelper.Views.Converters {
 
     public class BoolToVisibilityConverter : IValueConverter {
